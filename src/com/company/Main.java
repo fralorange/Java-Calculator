81a45fa789ffc2590c1bfa94e4bf27475dd231e4
fralorange <fralorange@mail.ru>
2021-10-12 19:49:48 +0300

    Added addition and subtraction methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
+class Calculator {
+    public static double Add(double a,double b) {
+        return a + b;
+    }
+    public static double Subtract(double a, double b) {
+        return a - b;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner s = new Scanner(System.in);
+        double a, b;
+        char operator;
+        System.out.print("Enter first number: ");
+        a = s.nextDouble();
+        System.out.println("Choose an operator : +, -, * or / :");
+        operator = s.next().charAt(0);
+        System.out.print("Enter second number: ");
+        b = s.nextDouble();
+        switch (operator) {
+            case '+' -> System.out.println(Calculator.Add(a,b));
+            case '-' -> System.out.println(Calculator.Subtract(a,b));
+            default -> System.out.println("Wrong operator!");
+        }
     }
 }
