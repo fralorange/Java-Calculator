68a0b09a79f3776b06c5eb3216938f6bfb34ec74
fralorange <fralorange@mail.ru>
2021-10-12 19:54:01 +0300

    Added multiplication and division methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,20 @@ package com.company;
 import java.util.Scanner;
 
 class Calculator {
-    public static double Add(double a,double b) {
+    public static double Add(double a, double b) {
         return a + b;
     }
+
     public static double Subtract(double a, double b) {
         return a - b;
+    }
+
+    public static double Multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b) {
+        return a / b;
     }
 }
 
@@ -23,8 +32,10 @@ public class Main {
         System.out.print("Enter second number: ");
         b = s.nextDouble();
         switch (operator) {
-            case '+' -> System.out.println(Calculator.Add(a,b));
-            case '-' -> System.out.println(Calculator.Subtract(a,b));
+            case '+' -> System.out.println(Calculator.Add(a, b));
+            case '-' -> System.out.println(Calculator.Subtract(a, b));
+            case '*' -> System.out.println(Calculator.Multiply(a, b));
+            case '/' -> System.out.println(Calculator.Divide(a, b));
             default -> System.out.println("Wrong operator!");
         }
     }
