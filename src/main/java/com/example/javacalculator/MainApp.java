bcc59dc65800505efee65940d5ac90d88ad412d0
fralorange <fralorange@mail.ru>
2021-10-16 14:13:31 +0300

    upd

--- src/main/java/com/example/javacalculator/MainApp.java
+++ src/main/java/com/example/javacalculator/MainApp.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("front.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         // Max-min height and width
         stage.setMinHeight(765.0);
